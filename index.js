9e204d374d809ae58bbd51f6c3c65fa66351196d
Viktoria <doduchenko29@gmail.com>
2021-07-05 12:01:27 +0300

    add

--- index.js
+++ index.js
@@ -1,69 +1,74 @@
 const baseUrl = 'https://60c9e9df772a760017204c05.mockapi.io/api/v1/form';
 
-const button = document.querySelector('.submit-button');
-const form = document.querySelector('.login-form');
-const inputEmail = document.querySelector('#email');
-const inputName = document.querySelector('#name');
-const inputPasword = document.querySelector('#password');
-const error = document.querySelector('.error-text');
+const buttonEl = document.querySelector('.submit-button');
+const formEl = document.querySelector('.login-form');
+const errorEl = document.querySelector('.error-text');
+
+const getFormData = () => Object.fromEntries(new FormData(formEl));
 // console.dir(button);
 // button activated
 // input: event
 // output: undefined
-const onValidateForm = () => {
-  console.log('123');
-  if (form.reportValidity()) {
-    button.disabled = false;
-  }
+const validateHandler = () => {
+  /*if (formEl.reportValidity()) {
+    buttonEl.disabled = false;
+  } else {
+    buttonEl.disabled = true;
+  }*/
+
+  buttonEl.disabled = !formEl.reportValidity();
+
+  //TODO
 };
-onValidateForm();
-form.addEventListener('input', onValidateForm);
+
 // get data for server
 // input:event
 // output: undeffined
-const getFormData = (event) => {
-  console.log('456');
+const submitHandler = (event) => {
   event.preventDefault();
-  const formData = [...new FormData(form)].reduce(
+
+  /*const formData = [...new FormData(form)].reduce(
     (acc, [field, value]) => ({
       ...acc,
       [field]: value,
     }),
     {}
-  );
+  );*/
+
   // const formData = `{${inputEmail.name}:${inputEmail.value}, ${inputName.name}:${inputName.value}, ${inputPasword.name}:${inputPasword.value}}`;
   // const formData = Object.fromEntries(new FormData(inputs));
-  console.dir(formData);
+
   fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify(getFormData()),
   })
-    //input: callback 
-      //input: response obj
-      //output: any
+    //input: callback
+    //input: response obj
+    //output: any
     //output: promise
 
     //response можна назвати будь-як
 
     //скільки асинхроних дій, стільки і then потрібно (тут два: записуємо дані на сервер, повертаємо обєкт)
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      alert(JSON.stringify(result));
-      inputEmail.value = '';
-      inputName.value = '';
-      inputPasword.value = '';
+    .then((response) => response.json())
+    .then((resultBody) => {
+      alert(JSON.stringify(resultBody));
+
+      formEl.reset();
+
+      //TODO use reset
     })
     .catch(() => {
-      error.textContent = 'Failed to create user';
+      errorEl.textContent = 'Failed to create user';
       // Promise.reject(new Error('Failed to create user'));
     });
 };
-form.addEventListener('submit', getFormData);
+
+formEl.addEventListener('input', validateHandler);
+formEl.addEventListener('submit', submitHandler);
 // input: event
 // output: undefined
 
